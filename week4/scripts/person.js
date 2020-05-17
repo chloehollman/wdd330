8b5ffa7f5fcef50ebf376166eeaec49c2eddbd0a
chloehollman <chollman@byui.edu>
2020-05-16 18:22:40 -0700

    new page

---
+++ week4/scripts/person.js
@@ -0,0 +1,8 @@
+export class Person {
+    construction(firstName, lastName, iNumber, location) {
+        this.firstName = first.Name,
+        this.lastName = lastName,
+        this.iNumber = iNumber,
+        this.location = location
+    }
+}
---
+++ week4/scripts/week3.js
@@ -0,0 +1,19 @@
+import { Person } from './person.js';
+
+let students = []
+
+let student1 =new Person['Tim', 'Thayne', '5555555', 'rexburg']
+let student2 =new Person['Bob', 'Thayne', '5555555', 'rexburg']
+let student1 =new Person['Bill', 'Thayne', '5555555', 'rexburg']
+let student1 =new Person['Sally', 'Thayne', '5555555', 'rexburg']
+
+students.push[student]
+
+students.forEach(
+    student => {
+        console.log($`(student.firstName) $(student.lastName)`);
+    }
+
+);
+
+let instr
