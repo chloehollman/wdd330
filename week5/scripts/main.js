8d2952044a4df131c241eeb5c3300db59fe5861f
chloehollman <chollman@byui.edu>
2020-05-19 15:01:41 -0700

    team activity

---
+++ week5/scripts/main.js
@@ -0,0 +1,4 @@
+import { Hike } from './hike.js';
+
+let hike1 = new Hike('Bechler Falls', 'falls.jpg','Image of Bechler Falls','3 miles','Easy', 'Beautiful short (or long) hike through Teton Canyon.','Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.')
+let hike2 = new Hike('Teton Canyon','falls.jpg','Image of Bechler Falls','3 miles','Easy')
