f49861444e574a1a3e526a7f1bf21adefe3cfd7e
chloehollman <chollman@byui.edu>
2020-05-23 17:04:28 -0700

    week 5 finished

--- week5/scripts/main.js
+++ week5/scripts/main.js
@@ -1,4 +1,17 @@
-import { Hike } from './hike.js';
+/* function declaration */
+// function toggleMenu() {
 
-let hike1 = new Hike('Bechler Falls', 'falls.jpg','Image of Bechler Falls','3 miles','Easy', 'Beautiful short (or long) hike through Teton Canyon.','Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.')
-let hike2 = new Hike('Teton Canyon','falls.jpg','Image of Bechler Falls','3 miles','Easy')
+// }
+
+// /* function expression */
+// const toggle = function() {
+    
+// }
+
+/* ----- arrow syntax ------*/
+
+const toggleMenu = () => {
+    document.querySelector("#navigation").classList.toggle("show");
+}
+
+document.querySelector("#menu").addEventListener("click", toggleMenu);
